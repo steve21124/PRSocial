984d1ee7ecc066b82e01e19cf91c0d3c9f8b5428
Elethom Hunter <elethomhunter@gmail.com>
2014-05-20 17:59:20 +0800

    Add Weibo auth

---
+++ Classes/Auth/Weibo/PRWeiboAuth.h
@@ -0,0 +1,18 @@
+//
+//  PRWeiboAuth.h
+//  PRSocialDemo
+//
+//  Created by Elethom Hunter on 5/20/14.
+//  Copyright (c) 2014 Project Rhinestone. All rights reserved.
+//
+
+#import "PRSocialAuth.h"
+#import "PRWeiboOAuth.h"
+
+@interface PRWeiboAuth : PRSocialAuth
+
+@property (nonatomic, assign) BOOL usesSSO;
+
+@property (nonatomic, readonly) NSString *accessToken;
+
+@end
---
+++ Classes/Auth/Weibo/PRWeiboAuth.m
@@ -0,0 +1,106 @@
+//
+//  PRWeiboAuth.m
+//  PRSocialDemo
+//
+//  Created by Elethom Hunter on 5/20/14.
+//  Copyright (c) 2014 Project Rhinestone. All rights reserved.
+//
+
+#import "PRWeiboAuth.h"
+#import <WeiboSDK.h>
+#import "PRWeiboService.h"
+
+@interface PRWeiboAuth () <WeiboSDKDelegate>
+
+@property (nonatomic, strong) PRSocialAuthCallback authCompletionHandler;
+
+@end
+
+@implementation PRWeiboAuth
+
+- (BOOL)isAuthorized
+{
+    return [PRWeiboOAuth sharedOAuth].isAuthorized;
+}
+
+- (void)authorizeWithCompletionHandler:(PRSocialAuthCallback)completion
+{
+    if (![PRWeiboOAuth sharedOAuth].isAuthorized && _usesSSO && [WeiboSDK isWeiboAppInstalled]) {
+        self.authCompletionHandler = completion;
+        [self sendSSOAuthRequest];
+    } else {
+        [[PRWeiboOAuth sharedOAuth] authorizeWithCompletionHandler:completion];
+    }
+}
+
+- (void)logout
+{
+    [[PRWeiboOAuth sharedOAuth] logout];
+}
+
+- (void)registerSSO
+{
+    [WeiboSDK registerApp:[[PRSocialConfig defaultConfig] valueForKey:kPRSocialConfigKeyAppID
+                                                       forServiceName:NSStringFromClass(PRWeiboService.class)]];
+}
+
+- (void)sendSSOAuthRequest
+{
+    WBAuthorizeRequest *request = [WBAuthorizeRequest request];
+    request.redirectURI = [[PRSocialConfig defaultConfig] valueForKey:kPRSocialConfigKeyOAuthRedirectURI
+                                                       forServiceName:NSStringFromClass(PRWeiboOAuth.class)];
+    request.scope = [[PRSocialConfig defaultConfig] valueForKey:kPRSocialConfigKeyOAuthScope
+                                                 forServiceName:NSStringFromClass(PRWeiboOAuth.class)];
+    [WeiboSDK sendRequest:request];
+}
+
+- (BOOL)handleSSOAuthOpenURL:(NSURL *)URL
+{
+    return [WeiboSDK handleOpenURL:URL delegate:self];
+}
+
+#pragma mark - Getters and setters
+
+- (NSString *)accessToken
+{
+    return [PRWeiboOAuth sharedOAuth].accessToken;
+}
+
+#pragma mark - Life cycle
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        self.usesSSO = YES;
+    }
+    return self;
+}
+
+#pragma mark - WeiboSDKDelegate
+
+- (void)didReceiveWeiboRequest:(WBBaseRequest *)request
+{
+    
+}
+
+- (void)didReceiveWeiboResponse:(WBBaseResponse *)response
+{
+    if ([response isKindOfClass:WBAuthorizeResponse.class]) {
+        WBAuthorizeResponse *authResponse = (WBAuthorizeResponse *)response;
+        if (response.statusCode == WeiboSDKResponseStatusCodeSuccess) {
+            PRWeiboOAuth *weiboOAuth = [PRWeiboOAuth sharedOAuth];
+            weiboOAuth.accessToken = authResponse.accessToken;
+            weiboOAuth.accessTokenTimeout = authResponse.expirationDate;
+            if (self.authCompletionHandler) {
+                self.authCompletionHandler(YES);
+            }
+        } else {
+            if (self.authCompletionHandler) {
+                self.authCompletionHandler(NO);
+            }
+        }
+    }
+}
+
+@end
