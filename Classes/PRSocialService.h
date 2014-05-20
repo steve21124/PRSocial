178014683f312645a571e3aac9340aeb87f7e5f4
Elethom Hunter <elethomhunter@gmail.com>
2014-05-20 10:02:48 +0800

    Add social service class

---
+++ Classes/PRSocialService.h
@@ -0,0 +1,22 @@
+//
+//  PRSocialService.h
+//  PRSocialDemo
+//
+//  Created by Elethom Hunter on 5/20/14.
+//  Copyright (c) 2014 Project Rhinestone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void (^PRSocialCallback)(BOOL success, NSDictionary *result);
+
+@interface PRSocialService : NSObject
+
++ (instancetype)sharedService;
+
+- (void)registerService;
+
+- (void)shareContentWithTitle:(NSString *)title description:(NSString *)description URL:(NSURL *)URL image:(UIImage *)image;
+- (void)shareContentWithTitle:(NSString *)title description:(NSString *)description URL:(NSURL *)URL image:(UIImage *)image completion:(PRSocialCallback)completion;
+
+@end
---
+++ Classes/PRSocialService.m
@@ -0,0 +1,53 @@
+//
+//  PRSocialService.m
+//  PRSocialDemo
+//
+//  Created by Elethom Hunter on 5/20/14.
+//  Copyright (c) 2014 Project Rhinestone. All rights reserved.
+//
+
+#import "PRSocialService.h"
+
+@implementation PRSocialService
+
++ (instancetype)sharedService
+{
+    static NSMutableDictionary *sharedServices;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedServices = [[NSMutableDictionary alloc] init];
+    });
+    NSString *classString = NSStringFromClass(self);
+    @synchronized(sharedServices) {
+        if (![sharedServices.allKeys containsObject:classString]) {
+            sharedServices[classString] = [[self alloc] init];
+        }
+    }
+    return sharedServices[classString];
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        [self registerService];
+    }
+    return self;
+}
+
+- (void)registerService
+{
+    
+}
+
+- (void)shareContentWithTitle:(NSString *)title description:(NSString *)description URL:(NSURL *)URL image:(UIImage *)image
+{
+    NSAssert(NO, @"%s Override needed on abstract method.", __PRETTY_FUNCTION__);
+}
+
+- (void)shareContentWithTitle:(NSString *)title description:(NSString *)description URL:(NSURL *)URL image:(UIImage *)image completion:(PRSocialCallback)completion
+{
+    NSAssert(NO, @"%s Override needed on abstract method.", __PRETTY_FUNCTION__);
+}
+
+@end
