4fd6c4cf482e4e6661225b9506ba7750cf87b727
Elethom Hunter <elethomhunter@gmail.com>
2014-05-20 11:53:38 +0800

    Add global header file

---
+++ Classes/PRSocial.h
@@ -0,0 +1,11 @@
+//
+//  PRSocial.h
+//  PRSocialDemo
+//
+//  Created by Elethom Hunter on 5/20/14.
+//  Copyright (c) 2014 Project Rhinestone. All rights reserved.
+//
+
+#import "PRSocialService.h"
+#import "PRSocialConfig.h"
+#import "PRWeChatService.h"
