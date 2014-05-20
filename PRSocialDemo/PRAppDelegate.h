3538d946bcb4029addc70ccde0b8562986010aed
Elethom Hunter <elethomhunter@gmail.com>
2014-05-20 09:25:35 +0800

    Initial Commit

---
+++ PRSocialDemo/PRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRAppDelegate.h
+//  PRSocialDemo
+//
+//  Created by Elethom Hunter on 5/20/14.
+//  Copyright (c) 2014 Project Rhinestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PRSocialDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PRSocialDemo
+//
+//  Created by Elethom Hunter on 5/20/14.
+//  Copyright (c) 2014 Project Rhinestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PRAppDelegate class]));
+    }
+}
